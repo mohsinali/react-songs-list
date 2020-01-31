7761cacdeec3a38f84581660da3a654eae55fb82
Mohsin <mohammadmohsin@gmail.com>
2020-01-31 08:28:41 +0500

    First commit with Redux

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { selectSong } from '../actions';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello App 33
+      </header>
+    </div>
+  );
+}
+
+export default App;
