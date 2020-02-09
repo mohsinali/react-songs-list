faecca9b72f081dc29ad01af0c9bdf84e181bb01
Mohsin Ali <mohammadmohsin@gmail.com>
2020-02-10 01:46:57 +0500

    Posts and users with memoized functions.

--- Projects/blog/src/actions/index.js
+++ Projects/blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 // export const fetchPosts = () => {
 //   return async function (dispatch, getState) {
@@ -9,6 +10,16 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 // Shortened syntax
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
-  dispatch({ type: 'FETCH_POSTS', payload: response });
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 }
 
+// export const fetchUser = userId => async dispatch => {
+//   const response = await jsonPlaceholder.get(`/users/${userId}`);
+//   dispatch( { type: 'FETCH_USER', payload: response.data } );
+// }
+
+export const fetchUser = userId => async dispatch => _fetchUser(userId, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch( { type: 'FETCH_USER', payload: response.data } );
+})
--- Projects/blog/src/components/PostList.js
+++ Projects/blog/src/components/PostList.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends Component {
   componentDidMount(){
     this.props.fetchPosts();
   }
 
+  renderList(){
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle alined icon user" />
+          <div className="content">
+            <div classNamek="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <UserHeader userId={post.userId} />
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render(){
-    return <div>PostList Component</div>;
+    return (
+      <div className="ui relaxed divided list">{this.renderList()}</div>
+    );
   }
 }
-
-export default connect(null, {fetchPosts})(PostList);
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+}
+export default connect(mapStateToProps, {fetchPosts})(PostList);
