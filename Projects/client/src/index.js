5c1518f04fae582a45ec3629ceb0a3bb62a9bc9b
Mohsin Ali <mohammadmohsin@gmail.com>
2020-02-17 21:24:30 +0500

    Saving login information in redux

--- Projects/client/src/index.js
+++ Projects/client/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+const store  = createStore(reducers, composeEnhancers());
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, 
+    document.getElementById('root')
+  );
