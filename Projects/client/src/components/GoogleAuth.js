11d68579605b23690ffb374d8bbf19469f313f78
Mohsin Ali <mohammadmohsin@gmail.com>
2020-02-17 14:12:46 +0500

    Google Auth done right.

---
+++ Projects/client/src/components/GoogleAuth.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: "888101207719-hfpvi29fjnk56v79heqb3jeuek02lkcu.apps.googleusercontent.com",
+        scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({isSignedIn: this.auth.isSignedIn.get()});
+        this.auth.isSignedIn.listen(this.onAuthChange);
+      });
+    })
+  }
+
+  onAuthChange = () => {
+    this.setState({isSignedIn: this.auth.isSignedIn.get()});
+  }
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  }
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+      
+    }else if(this.state.isSignedIn){
+      return (
+        <div>
+          <button onClick={this.onSignOutClick} className="ui red google button">
+            <i className="google icon" />
+            Sign Out
+          </button>
+        </div>
+      )
+
+    }else {
+      return (
+        <div>
+          <button onClick={this.onSignInClick} className="ui red google button">
+            <i className="google icon" />
+            Sign In with Google
+          </button>
+        </div>
+      )
+
+    }
+  }
+
+  render(){
+    return (
+      <div>{this.renderAuthButton()}</div>
+    );
+  }
+}
+
+export default GoogleAuth;
---
+++ Projects/client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
+
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <div className="right menu">
+        <Link to="/" className="item">Streamer</Link>
+      </div>
+
+      <div className="right menu">
+        <Link to="/" className="item">All Streams</Link>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
