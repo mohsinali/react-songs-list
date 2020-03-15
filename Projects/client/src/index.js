58cacdcda9341185076060368863e5c46e3572a0
Mohsin Ali <mohammadmohsin@gmail.com>
2020-03-16 00:51:26 +0500

    Integrating redux-thunk and saving data in db.json.

--- Projects/client/src/index.js
+++ Projects/client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import readuxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-const store  = createStore(reducers, composeEnhancers());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store  = createStore(reducers, composeEnhancers(applyMiddleware(readuxThunk)));
 
 ReactDOM.render(
     <Provider store={store}>
