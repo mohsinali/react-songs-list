939acda85ab1a7766291f2a09f99e477f8807331
Mohsin Ali <mohammadmohsin@gmail.com>
2020-03-24 03:54:28 +0500

    Creating streams with userId.

--- Projects/client/src/actions/index.js
+++ Projects/client/src/actions/index.js
@@ -29,8 +29,9 @@ export const signOut = () => {
 // }
 
 // Aleternate short syntax
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const {userId} = getState().auth;
+  const response = await streams.post('/streams', {...formValues, userId});
   dispatch({type: CREATE_STREAM, payload: response.data});
 }
 
--- Projects/client/src/components/streams/StreamList.js
+++ Projects/client/src/components/streams/StreamList.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends React.Component {
+  componentDidMount(){
+    this.props.fetchStreams();
+  }
+
+  renderList(){
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+          </div>
+          <div className="description">{stream.description}</div>
+        </div>
+      )
+    })
+  }
+  
+  render(){
+    return (
+      <div>
+        {this.renderList()}
+      </div>
+    );
+  }
 };
 
-export default StreamList;
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) };
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
